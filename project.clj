af885a723c00565019e94cca284e3213e89febd7
Jakub Holy <jakub.holy@telia.no>
2018-09-23 07:44:47 +0200

    Replace all with `gatsby new <app>`

--- project.clj
+++
@@ -1,5 +0,0 @@
-; dummy file for IntelliJ integration
-(defproject rapidshop-b2b-shopping "0.1.0-SNAPSHOT"
-  :dependencies [[thheller/shadow-cljs "X.Y.Z"]]
-  ;:source-paths ["src"] ; set in shadow-cljs.edn
-  )
